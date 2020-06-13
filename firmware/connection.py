1d197ff947bc16d54deada30957a07e22e67d003
Robert Smith <rsmith5901@gmail.com>
2020-06-12 22:26:05 -0400

    connection firmware

---
+++ firmware/connection.py
@@ -0,0 +1,20 @@
+from umqtt.simple import MQTTClient
+import time, network
+
+
+def connect(client_id):
+    conn = network.Cellular()
+
+    aws_endpoint = 'a24e0i570zhegg.iot.us-east-2.amazonaws.com'
+
+    ssl_params = {'keyfile': "/flash/cert/aws.key",
+                  'certfile': "/flash/cert/aws.crt",
+                  'ca_certs': "/flash/cert/aws.ca"}
+
+    while not conn.isconnected():
+        print("waiting for network connection...")
+        time.sleep(4)
+
+    c = MQTTClient(client_id, aws_endpoint, ssl=True, ssl_params=ssl_params)
+    c.connect()
+    return c
--- firmware/main.py
+++ firmware/main.py
@@ -1,5 +1,42 @@
-# Default template for XBee MicroPython projects
+import time
+from connection import connect
+from machine import Pin
 
-import umqtt
+msgs_received = 0
 
-print("Hello World!")
+dio10 = Pin("P0", Pin.OUT, value=0)
+
+print("network connected")
+
+client_id = "oasidfjoasidfjasdf"
+c = connect(client_id)
+
+
+def sub_cb(topic, msg):
+    global msgs_received
+    msgs_received += 1
+    print(topic, msg)
+    dio10.toggle()
+
+
+def subscribe():
+    c.set_callback(sub_cb)
+    c.subscribe("sample/xbee")
+
+    global msgs_received
+
+    msgs_received = 0
+    while msgs_received < 5:
+        c.check_msg()
+        time.sleep(5)
+    time.sleep(1)
+    c.disconnect()
+    print("DONE")
+
+
+def publish_test():
+    c.publish("sample/xbee", '{"message": "Yay from Robert\'s Xbee"}')
+
+
+publish_test()
+subscribe()
