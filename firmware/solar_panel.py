925bdcda9c5f34a52965ccd804ba61544bd20e2c
Robert Smith <rsmith5901@gmail.com>
2020-06-14 12:04:30 -0400

    capturing panel state

--- firmware/solar_panel.py
+++ firmware/solar_panel.py
@@ -1,10 +1,14 @@
+import machine
+import json
+
+
 class SolarPanel:
     def __init__(self, panel_id, mqtt_connection):
         self.id = panel_id
         self.mqtt_connection = mqtt_connection
 
     def publish_current_state(self):
-        current_state = '{"' + self.__get_panel_id() + '": "Encoded data"}'
+        current_state = '{"' + self.__get_panel_id() + '": "' + json.dumps(self.__get_current_state()) + '"}'
         current_state_topic = "solar-panel/state"
 
         print("--- start publish panel state ---")
@@ -13,6 +17,13 @@ class SolarPanel:
 
         self.mqtt_connection.publish(current_state_topic, current_state)
         print("--- end publish panel state ---")
+
+    def __get_current_state(self):
+        analog_1 = machine.ADC('D1')
+
+        return {
+            "analog-one": analog_1.read()
+        }
 
     def __get_panel_id(self):
         delimiter = '-'
